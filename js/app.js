c2e476b984063bf4b1d2d556ac282978ed952a1a
Sheryl Lee <sheryl_lee_wl@yahoo.com.sg>
2020-08-29 17:34:51 +1000

    Created nav bar list items

--- js/app.js
+++ js/app.js
@@ -1,28 +1,29 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
 */
 
 /**
  * Define Global Variables
- * 
+ *
 */
-
+const nav = document.getElementById('navbar__list');
+const sections = document.querySelectorAll('section');
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
+ *
 */
 
 
@@ -30,11 +31,21 @@
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
 
 // build the nav
 
+function buildNavBar() {
+  for(let i=0; i<sections.length; i++){
+    const section = sections[i]
+    const tab = document.createElement('li');
+    tab.innerHTML = section.getAttribute('data-nav');
+    nav.appendChild(tab);
+  }
+};
+
+buildNavBar();
 
 // Add class 'active' to section when near top of viewport
 
@@ -45,13 +56,11 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
